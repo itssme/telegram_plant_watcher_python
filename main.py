da9d5090323ddef00396baad31942c4fec878f55
itssme <itssme3000@gmail.com>
2020-02-25 21:00:26 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,158 @@
+from telegram.ext import Updater, CommandHandler
+import systemd.daemon
+import datetime
+import telegram
+import logging
+import sqlite3
+import time
+import cv2
+import os
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+chat_ids = []
+
+
+def get_frame(vs):
+    start_time = time.time()
+    frame = None
+    while time.time() - start_time <= 4 or frame is None:
+        frame = vs.read()[1]
+
+    return frame
+
+
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Usage:\n"
+                                                                    "/help - prints this help\n"
+                                                                    "/register - saves your chat id for getting "
+                                                                    "messages")
+
+
+def add_chat_id(update, context):
+    chat_id = update.message.chat_id
+    db = sqlite3.connect("chats.db")
+    cur = db.cursor()
+    cur.execute("insert into chats values(?);", [chat_id])
+    print(cur.fetchall())
+    cur.close()
+    db.commit()
+    db.close()
+    print("inserted")
+    update_ids()
+
+
+def update_ids():
+    global chat_ids
+    db = sqlite3.connect("chats.db")
+    cur = db.cursor()
+    cur.execute("select * from chats;")
+    chat_ids = [chat_id[0] for chat_id in cur.fetchall()]
+    cur.close()
+    print(chat_ids)
+    print("[!] updated ids")
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    global chat_ids
+
+    db = None
+    if not os.path.isfile("chats.db"):
+        db = sqlite3.connect("chats.db")
+        cur = db.cursor()
+        cur.executescript("""create table chats (id varchar(256) primary key);""")
+        cur.close()
+    else:
+        db = sqlite3.connect("chats.db")
+
+    cur = db.cursor()
+    cur.execute("select * from chats;")
+    chat_ids = [chat_id[0] for chat_id in cur.fetchall()]
+    cur.close()
+    print(chat_ids)
+
+    bot = telegram.Bot(token=os.environ["bot_token"])
+
+    def send_all(text):
+        for chat_id in chat_ids:
+            bot.send_message(chat_id=chat_id, text=text)
+
+    def send_all_photo(filename):
+        for chat_id in chat_ids:
+            bot.send_photo(chat_id=chat_id, photo=open(filename, 'rb'))
+
+    send_all("I am online again and stalking your plant (﻿ ͡° ͜ʖ ͡°)")
+
+    updater = Updater(os.environ["bot_token"], use_context=True)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("register", add_chat_id,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    dp.add_handler(CommandHandler("help", help,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    dp.add_error_handler(error)
+    updater.start_polling()
+
+    systemd.daemon.notify('READY=1')
+
+    try:
+        vs = cv2.VideoCapture(0)
+
+        while True:
+            current_time = datetime.datetime.now()
+            next_date = datetime.datetime(current_time.year, current_time.month, current_time.day, hour=12, minute=0,
+                                          second=0, microsecond=0)
+            wait_time = next_date - current_time
+            due = wait_time.seconds
+            print("Waiting for " + str(due) + " seconds")
+
+            if due < 0:
+                send_all("There was an error in getting the next time")
+                return
+
+            time.sleep(due)
+            frame = get_frame(vs)
+            filename = os.environ["working_dir"] + "/" + str(datetime.datetime.now()).replace(".", "_").replace(" ",
+                                                                                                          "_") + ".jpg"
+            return_value = cv2.imwrite(filename, frame)
+            send_all_photo(filename)
+
+            if return_value:
+                send_all("Image saved " + str(filename))
+            else:
+                send_all("Failed to save image :( " + str(filename))
+            print(return_value)
+            print("saved frame")
+
+            # save a second image on the same day
+            time.sleep(60*60)
+
+            try:
+                frame = get_frame(vs)
+                filename = os.environ["working_dir"] + "/" + str(datetime.datetime.now()).replace(".", "_").\
+                    replace(" ", "_") + ".jpg"
+
+                return_value = cv2.imwrite(filename, frame)
+                print(return_value)
+                print("saved frame")
+            except Exception as e:
+                print(e)
+                print("Unable to save second image :/")
+
+    except Exception as e:
+        send_all("Unknown error " + str(e))
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,118 @@
+import setuptools
+import os
+import subprocess
+
+
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+if os.getuid() != 0:
+    print(Colors.FAIL + "[!] please run this program as root for installation" + Colors.ENDC)
+    quit(1)
+
+print(Colors.OKGREEN + "[!] installing python3-systemd via apt" + Colors.ENDC)
+child = subprocess.Popen(["apt-get", "install", "python3-systemd", "-y"])
+streamdata = child.communicate()[0]
+rc = child.returncode
+
+if rc != 0:
+    print(Colors.FAIL + "[!] failed to run apt install python3-systemd")
+    quit(1)
+
+print(Colors.OKGREEN + "[!] installed python3-systemd successfully" + Colors.ENDC)
+
+print(Colors.OKGREEN + "[!] creating working directory in /home/plant_watcher" + Colors.ENDC)
+child = subprocess.Popen(["mkdir", "/home/plant_watcher", "--parent"])
+streamdata = child.communicate()[0]
+rc = child.returncode
+
+if rc != 0:
+    print(Colors.FAIL + "[!] failed create directory /home/plant_watcher")
+    quit(1)
+
+child = subprocess.Popen(["chmod", "777", "/home/plant_watcher"])
+streamdata = child.communicate()[0]
+rc = child.returncode
+
+if rc != 0:
+    print(Colors.FAIL + "[!] failed to change permissions of working directory" + Colors.ENDC)
+    quit(1)
+
+print(Colors.OKBLUE + "[!] Type in your bot token for telegram" + Colors.ENDC)
+
+service_file_writer = open(os.path.dirname(os.path.abspath(__file__)) + "/plant_watcher.service", "w")
+
+service_file = """[Unit]
+Description=Monitoring service
+[Service]
+ExecStart=/usr/bin/python3 """ + os.path.dirname(os.path.abspath(__file__)) + """/main.py
+Environment="PYTHONUNBUFFERED=1"
+Environment="take_picture=12h00m"
+Environment="bot_token=""" + input("?: ") + """"
+Environment="working_dir=/home/plant_watcher"
+Environment="debug=False"
+Restart=on-failure
+Type=notify
+[Install]
+WantedBy=default.target"""
+
+service_file_writer.write(service_file)
+service_file_writer.close()
+
+child = subprocess.Popen(["cp", "plant_watcher.service", "/etc/systemd/system/plant_watcher.service"])
+streamdata = child.communicate()[0]
+rc = child.returncode
+
+if rc != 0:
+    print(Colors.FAIL + "[!] failed to move service file to systemd directory" + Colors.ENDC)
+    quit(1)
+
+child = subprocess.Popen(["chown", "root:root", "/etc/systemd/system/plant_watcher.service"])
+streamdata = child.communicate()[0]
+rc = child.returncode
+
+if rc != 0:
+    print(Colors.FAIL + "[!] failed to change owner of service file to root " + Colors.ENDC)
+    quit(1)
+
+child = subprocess.Popen(["chmod", "644", "/etc/systemd/system/plant_watcher.service"])
+streamdata = child.communicate()[0]
+rc = child.returncode
+
+if rc != 0:
+    print(Colors.FAIL + "[!] failed to change permissions of service file" + Colors.ENDC)
+    quit(1)
+
+base_dir = os.path.dirname(__file__)
+readme_path = os.path.join(base_dir, "readme.md")
+if os.path.exists(readme_path):
+    with open(readme_path) as stream:
+        long_description = stream.read()
+else:
+    long_description = ""
+
+setuptools.setup(
+    name="plant_watcher",
+    version="0.0.1",
+    author="itssme",
+    author_email="itssme3000@gmail.com",
+    description="Python bot that sends images of my plant to me in telegram",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/itssme/plant_watcher",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
